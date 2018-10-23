c81844cc93038c6f90ccb22db7b645be126d396a
hreilly <hreilly@gvhomes.com>
2018-10-23 08:31:42 -0700

    Update Python/fcrov_data_collection.py

--- Python/fcrov_data_collection.py
+++ Python/fcrov_data_collection.py
@@ -20,4 +20,42 @@ df = pd.concat(dfs[2:10])
 df.to_json('fcrov_data.json', orient='records')
     
     #time.sleep(60.0 - ((time.time() - starttime) % 60.0)) # Update data in 60 sec. intervals since start of last data update.
+
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+import pandas as pd
+import numpy as np
+import requests
+
+res = requests.get(
+    "https://www.co.fresno.ca.us/departments/county-clerk-registrar-of-voters/election-information/election-results/results-for-june-5-2018-statewide-primary-election")
+
+REMOVE_ATTRIBUTES = [
+    'style','style','class','border','align','cellpadding',
+    'cellspacing','colspan','width']
+REMOVE_TAGS = ['hr/']
+
+# Create filter with SoupStrainer to limit parsing to main div
+res_filter = SoupStrainer('div',{'id': 'widget_328_7137_5329'})
+
+soup = BeautifulSoup(res.content,'lxml',parse_only=res_filter)
+
+# Filter out <hr>
+[x.decompose() for x in soup.findAll('hr')]
+
+# Filter out <p>
+[x.decompose() for x in soup.findAll('p')]
+
+# Look for attributes and remove, then pass
+for attribute in REMOVE_ATTRIBUTES:
+    for tag in soup.find_all():
+        del(tag[attribute])
+        
+# Filter out empty "td" tags
+# empty_td_tags = soup.findAll(lambda tag: tag.name == 'td' and not tag.contents and (tag.string is None or not tag.string.strip()))
+# [empty_tag.decompose() for empty_tag in empty_td_tags]
+
+# Print the final result of all filters and functions
+with open("fcrov_data.html", "w") as file:
+    file.write(str(soup))
     
