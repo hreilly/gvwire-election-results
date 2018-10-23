d20420477bbc7498dddb65d04f2663f5fd5e7170
hreilly <hannahreillyca@gmail.com>
2018-10-23 13:25:52 -0700

    Parsing data called by Python script/output to data folder

--- Python/fcrov_data_collection.py
+++ Python/fcrov_data_collection.py
@@ -1,61 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 08:32:22 2018
+
+@author: hreilly
+"""
+
 #import time
 #starttime=time.time()
 
 #while True:
     
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-
-# Highest level content
-
-source = requests.get("https://www.co.fresno.ca.us/departments/county-clerk-registrar-of-voters/election-information/election-results/results-for-june-5-2018-statewide-primary-election") # Grab data from the Fresno County Registrar of Voters website
-soup = BeautifulSoup(source.text,'lxml') # Grab the whole page with BS4
-data = soup.find(id="widget_328_7137_5329") # Locate main results div
-
-# Handling data
-
-dfs = pd.read_html(str(data))
-df = pd.concat(dfs[2:10])
-df.to_json('fcrov_data.json', orient='records')
-    
-    #time.sleep(60.0 - ((time.time() - starttime) % 60.0)) # Update data in 60 sec. intervals since start of last data update.
-
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 import pandas as pd
-import numpy as np
 import requests
+import re
+import json
 
+"""
+Prepping the ingredients / highest level elements
+"""
+
+# Initial request to Fresno County site
 res = requests.get(
     "https://www.co.fresno.ca.us/departments/county-clerk-registrar-of-voters/election-information/election-results/results-for-june-5-2018-statewide-primary-election")
-
-REMOVE_ATTRIBUTES = [
-    'style','style','class','border','align','cellpadding',
-    'cellspacing','colspan','width']
-REMOVE_TAGS = ['hr/']
 
 # Create filter with SoupStrainer to limit parsing to main div
 res_filter = SoupStrainer('div',{'id': 'widget_328_7137_5329'})
 
+# Grab the strained soup
 soup = BeautifulSoup(res.content,'lxml',parse_only=res_filter)
+
+"""
+Cooking soup / breaking down content / Print to HTML file for analysis
+"""
+
+REMOVE_ATTRIBUTES = [
+    'style','style','class','border','align','valign','cellpadding',
+    'cellspacing','colspan','width']
 
 # Filter out <hr>
 [x.decompose() for x in soup.findAll('hr')]
 
+# Filter out <hr>
+[x.decompose() for x in soup.findAll('br')]
+
 # Filter out <p>
 [x.decompose() for x in soup.findAll('p')]
+
+# Filter out <h3>
+[x.decompose() for x in soup.findAll('h3')]
+
+# Filter out <h2>
+[x.decompose() for x in soup.findAll('h2')]
+
+# Filter out <link>
+[x.decompose() for x in soup.findAll('link')]
 
 # Look for attributes and remove, then pass
 for attribute in REMOVE_ATTRIBUTES:
     for tag in soup.find_all():
         del(tag[attribute])
         
-# Filter out empty "td" tags
-# empty_td_tags = soup.findAll(lambda tag: tag.name == 'td' and not tag.contents and (tag.string is None or not tag.string.strip()))
-# [empty_tag.decompose() for empty_tag in empty_td_tags]
+# Clean up odd formatting of number percents
+        
+vote_prcnt_ptrn = re.compile(r'\d+[.]\d+%')
 
 # Print the final result of all filters and functions
-with open("fcrov_data.html", "w") as file:
-    file.write(str(soup))
+with open("../data/fcrov_data.html", "w") as file:
+    file.write(soup.prettify())
+
+# Convert soup to data frame objects
+dfs = pd.read_html(str(soup), header=0)
+
+# Naming relevant data frames
+city_council_3 = dfs[64]
+city_council_5 = dfs[66]
+city_council_7 = dfs[68]
+
+# Var for naming cols
+new_cols = {'Unnamed: 1':'partyPref', 'Unnamed: 2':'voteNum', 'Unnamed: 3':'votePrcnt'}
+
+# Rename columns in single data frames
+city_council_5.rename(columns = new_cols, inplace = True)
+city_council_3.rename(columns = new_cols, inplace = True)
+city_council_7.rename(columns = new_cols, inplace = True)
+
+list_df = [city_council_3, city_council_5, city_council_7]
+
+print(list_df)
+
+with open("../data/fcrov_data.json", 'w') as outfile:
+    outfile.write(json.dumps([dfs.to_dict() for dfs in list_df]))
     
+    #time.sleep(120.0 - ((time.time() - starttime) % 120.0)) # Update data in 60 sec. intervals since start of last data update.
+
+
+
+
+
+
+
